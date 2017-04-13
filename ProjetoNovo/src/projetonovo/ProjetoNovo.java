90c8627f8c771aadc1fe560557f68d0cdef03237
93709749204 <93709749204@JIPA-LABIN05-26.ifro.local>
2017-04-12 21:35:58 -0400

    Meu Primeiro commit

--- ProjetoNovo/src/projetonovo/ProjetoNovo.java
+++ ProjetoNovo/src/projetonovo/ProjetoNovo.java
@@ -1,12 +1,15 @@
 
 package projetonovo;
 
+import javax.swing.JOptionPane;
+
 
 public class ProjetoNovo {
 
    
     public static void main(String[] args) {
-        
+        String menssagen = " Olá Mundo!!!! ";
+       JOptionPane .showMessageDialog(null, menssagen);
     }
     
 }
